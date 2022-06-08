e76fdf2065d7a30fa5d452df5256b36ba9d9cc3e
CyniteOfficial <107128247+CyniteOfficial@users.noreply.github.com>
2022-06-08 21:01:46 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | Modifieded By : @DC4_WARRIOR
+# (c) Shrimadhav U K | Modifieded By : @CyniteOfficial
 
 # the logging things
 import logging
